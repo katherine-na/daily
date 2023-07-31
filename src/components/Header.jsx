60c396dcf67b604cf6527125e9ef94c1094b7484
Katherine <katherine@vinneren.com.mx>
2023-07-31 10:51:52 -0600

    Add features

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react';
 import MoonIcon from './icons/MoonIcon';
+import SunIcon from './icons/SunIcon';
 
 const Header = () => {
+    const [darkMode, setDarkMode] = useState(true);
+
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [darkMode]);
+
     return (
         <header className="container mx-auto px-4 pt-8">
             <div className="flex justify-between">
                 <h1 className="text-white text-3x1 font-semibold uppercase tracking-[]0.3em">Things to do</h1>
-                <button>
-                    <MoonIcon />
-                </button>
+                <button onClick={() => setDarkMode(!darkMode)}>{darkMode ? <SunIcon /> : <MoonIcon />}</button>
             </div>
         </header>
     );
